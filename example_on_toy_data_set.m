1a3db35107e442552a79228598981f2c9219f134
Roee Gilron <roeegilron@gmail.com>
2016-04-20 22:50:52 +0300

    example data set

---
+++ example_on_toy_data_set.m
@@ -0,0 +1,13 @@
+function example_on_toy_data_set()
+%% Create a demo of multi-T and FuA on a toy data set to better understand how they work
+
+% Create Toy Data
+% Visualize Toy Data 
+% Compute Multi T 
+% Compute FuA 
+% Visualize Multi T 
+% Visualize FuA 
+
+
+end
+
